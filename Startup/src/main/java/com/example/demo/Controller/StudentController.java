7b72d63ab0efb4426e361806a09c3fedc90326b2
balaanthu <119047499+balaanthu@users.noreply.github.com>
2023-02-23 14:57:02 +0530

    Add files via upload

---
+++ Startup/src/main/java/com/example/demo/Controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.demo.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.StudentDemo;
+import com.example.demo.Service.StudentService;
+
+@RestController
+public class StudentController {
+     @Autowired(required=true)
+     StudentService stuService;
+     @PostMapping("/addDetails")
+     public StudentDemo addInfo(@RequestBody StudentDemo st) {
+    	 return stuService.saveDetails(st);
+     }
+     @GetMapping("/getDetails")
+     public List<StudentDemo> fetchDetails(){
+    	 return stuService.getDetails();
+     }
+     @PutMapping("/updateDetails")
+     public StudentDemo updateInfo(@RequestBody StudentDemo st1) {
+    	 return stuService.saveDetails(st1);
+     }
+     @DeleteMapping("/deleteDetails/{stu_id}")
+     public  String deleteInfo(@PathVariable("stu_id")int stu_id)
+      {
+ 		stuService.deleteDetails(stu_id);
+     	 return "Deleted the Details";
+      }
+}
